2b8bacc63d3938c17ac59a75d838ba2104fbe8f3
Zsolt Sandor <sz332@users.noreply.github.com>
2020-06-26 21:22:18 +0200

    initial commit

--- src/main/java/com/acme/model/impl/UMLOperationVisitor.java
+++ src/main/java/com/acme/model/impl/UMLOperationVisitor.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.acme.model.UMLOperation;
 
+@FunctionalInterface
 public interface UMLOperationVisitor {
     
     void visit(UMLOperation operation, List<UMLOperation> path);
---
+++ src/main/java/com/acme/verification/confroles/ConflictingRolesVerification.java
@@ -0,0 +1,45 @@
+package com.acme.verification.confroles;
+
+import java.util.List;
+
+import com.acme.model.UMLInterface;
+import com.acme.model.UMLModel;
+import com.acme.model.UMLOperation;
+import com.acme.model.UMLRole;
+import com.acme.model.impl.UMLOperationWalker;
+
+public class ConflictingRolesVerification {
+
+    private final UMLModel model;
+
+    public ConflictingRolesVerification(UMLModel model) {
+        this.model = model;
+    }
+
+    public ConflictingRolesVerificationResult verify() {
+
+        ConflictingRolesVerificationResult result = new ConflictingRolesVerificationResult();
+
+        for (UMLInterface umlInterface : model.interfaces()) {
+            processInterface(umlInterface, result);
+        }
+
+        return result;
+    }
+
+    private void processInterface(UMLInterface umlInterface, ConflictingRolesVerificationResult result) {
+        ConflictingRolesPolicy policy = new ConflictingRolesPolicy();
+        UMLOperationWalker walker = new UMLOperationWalker();
+
+        for (final UMLOperation rootOperation : umlInterface.operations()) {
+            walker.walk(rootOperation, (currentOperation, path) -> {
+                List<UMLRole> missingRoles = policy.compare(rootOperation.roles(), currentOperation);
+
+                if (!missingRoles.isEmpty()) {
+                    result.addConflict(missingRoles, path);
+                }
+            });
+        }
+    }
+
+}
