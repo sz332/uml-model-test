4147f82c0fc18ad9383daa0a747f401d80e3211d
Zsolt Sandor <sz332@users.noreply.github.com>
2020-06-26 23:42:04 +0200

    test code for uml model builder

---
+++ src/test/java/com/acme/verification/confroles/ConflictingRolesVerification.java
@@ -0,0 +1,11 @@
+package com.acme.verification.confroles;
+
+import com.acme.model.UMLModel;
+
+import org.junit.Before;
+
+public class ConflictingRolesVerification {
+
+
+    
+}
---
+++ src/test/java/com/acme/verification/confroles/UMLModelBuilder.java
@@ -0,0 +1,16 @@
+package com.acme.verification.confroles;
+
+/**
+ * A simple class to build an UML model for testing purposes
+ */
+public class UMLModelBuilder {
+
+    public UMLModelBuilder(){
+
+    }
+    
+    // new DummyModel(
+    // new DummyInterface("name", Arrays.asList(new DummyOperationPath("",""), new DummyOperationPath("","","")))
+    // ).asModel()
+        
+}
